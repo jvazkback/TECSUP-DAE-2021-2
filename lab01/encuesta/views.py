f66eaa696940d906e50a24fcc56a38a13b95b797
jvazkback <jvazkback@hotmail.com>
2021-08-18 11:30:49 -0500

    lab01

---
+++ lab01/encuesta/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def index(request):
+    return HttpResponse("saludos desde la vista encuestas")
+
+def detalle(request, pregunta_id):
+    return HttpResponse("Estas viendo la pregunta %s." % pregunta_id)
+
+def resultados(request, pregunta_id):
+    response = "Estas viendo los resultados de la pregunta %s"
+    return HttpResponse(response % pregunta_id)
+
+def votar(request, pregunta_id):
+    return HttpResponse("Estas votando por la pregunta %s." % pregunta_id)
